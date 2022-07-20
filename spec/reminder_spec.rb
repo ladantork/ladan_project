3d43b3c8115b49d2e65b548988bd9fd267b04c56
ladantork <ladantorkzaban0@gmail.com>
2022-07-20 14:31:37 +0100

    Testing For Errors(introduction)

--- spec/reminder_spec.rb
+++ spec/reminder_spec.rb
@@ -2,10 +2,10 @@ require 'reminder'
 
 # We use the class name here rather than a string
 RSpec.describe Reminder do
-  it "reminds the user to do a task" do
+  context "when no task is set" do
+  it "fails" do
     reminder = Reminder.new("Kay")
-    reminder.remind_me_to("Walk the dog")
-    result = reminder.remind()
-    expect(result).to eq "Walk the dog, Kay!"
+    expect{reminder.remind()}.to raise_error "No reminder set!"
   end
+end
 end
