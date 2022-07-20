375bec793bf9ebd65e5cce2ee6d6fae8690c6fe1
ladantork <ladantorkzaban0@gmail.com>
2022-07-20 15:12:48 +0100

    Testing For Errors ( Exercise )

---
+++ Users/evelina/Just-Enough-Git-to-Get-Started/project1/present_spec.rb
@@ -0,0 +1,20 @@
+require "present"
+
+RSpec.describe Present do
+  it "returns wrap contents " do
+
+    present = Present.new 
+    result = present.wrap ("toy")
+    expect(present.unwrap).to eq "toy"
+
+  end
+  it "if unwrap without wrapping first" do
+    present = Present.new
+    expect { present.unwrap }.to raise_error "No contents have been wrapped."
+  end
+  it " " do
+    present = Present.new
+    present.wrap ("toy")
+    expect { present.wrap ("same") }.to raise_error "A contents has already been wrapped." 
+  end
+end
